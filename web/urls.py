3d4720ecdd203fc356298c81300b4df10d7d8646
paiain <86291726+paiain@users.noreply.github.com>
2021-07-23 19:37:53 +0700

    upload

---
+++ web/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path 
+from web.views import about, contact, index
+
+urlpatterns = [
+   
+    path('',index, name='index'),
+    path('about/',about, name='about'),
+    path('contact',contact, name='contact'),
+]
